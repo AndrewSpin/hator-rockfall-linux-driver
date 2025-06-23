0a4b489bef5211ca8fdf39c492371f19841028c1
andrew <a.pyshnenko@snov.io>
2025-06-23 15:35:13 +0300

    first commit

---
+++ keyboard_change_color.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+KEYBOARD_COLOR="$1"
+HIDRAW_IDENTIFIER="0416:A0F8"
+HIDRAW_SIGNAL="06be15000101020605000000${KEYBOARD_COLOR}0000000000000000000000000000000000"
+
+find_my_hidraw_device() {
+  for dev in $(ls /dev/hidraw* | sort -r); do
+    if [ -e "$dev" ]; then
+        result=$(udevadm info "$dev" | grep "$HIDRAW_IDENTIFIER")
+        if [ -n "$result" ]; then
+            echo "$dev"
+            return 0
+        fi
+    fi
+  done
+
+  echo "Device was not found" >&2
+  exit 1
+}
+
+echo $HIDRAW_SIGNAL | xxd -r -p > $(find_my_hidraw_device)
+
+exit 1
